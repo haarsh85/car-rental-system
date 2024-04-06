3c25717ec72b8b4e96d4f1f4010d8eef5077d92b
Harsha Nanayakkara <82449271+haarsh85@users.noreply.github.com>
2024-04-07 00:49:40 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,24 @@
+package Test1;
+
+public class Car extends Vehicle{
+	
+	private int numDoors;
+	private boolean isConvertible;
+	private String fuelType;
+	
+	public Car(String make, String model, int year, double rentalRate, int numDoors, boolean isConvertible, String fuelType) {
+		super(make, model, year, rentalRate);
+		this.numDoors = numDoors;
+		this.isConvertible = isConvertible;
+		this.fuelType = fuelType;
+	}
+	
+	@Override
+	public void displayInfo() {
+		super.displayInfo();
+		System.out.println("Number of Doors: " + numDoors);
+		System.out.println("Convertible: " + isConvertible);
+		System.out.println("Fuel Type: " + fuelType);
+	}
+
+}
---
+++ Motorbike.java
@@ -0,0 +1,18 @@
+package Test1;
+
+public class Motorbike extends Vehicle{
+	
+	private String bikeType;
+	
+	public Motorbike(String make, String model, int year, double rentalRate, String bikeType) {
+		super(make, model, year, rentalRate);
+		this.bikeType = bikeType;
+	}
+	
+	@Override
+	public void displayInfo() {
+		super.displayInfo();
+		System.out.println("Bike Type" + bikeType);
+	}
+
+}
